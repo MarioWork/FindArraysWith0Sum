8cd4a4a5d87d8db8f8a87bbd84abaa2992459cec
Mario Vieria <47696178+MarioWork@users.noreply.github.com>
2021-01-05 12:01:12 -0100

    Finished
    Algorithm completed.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindArraysWith0Sum
+{
+    class Program
+    {
+        //Print all sub-arrays with 0 sum
+        //Given an array of integers, print all sub-arrays with 0 sum.
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 2, -3, -1, 0, 4 };
+            GetSubArrays(numbers);
+        }
+
+
+        public static void GetSubArrays(int[] numbs)
+        {
+            for (int i = 0; i < numbs.Length; i++)
+            {
+                int sum = 0;
+
+                for (int j = i; j < numbs.Length; j++)
+                {
+                    sum += numbs[j];
+
+                    if (sum == 0)
+                    {
+                        Console.WriteLine($"Subarray[{i}...{j}]");
+                    }
+                }
+
+            }
+
+
+
+        }
+    }
+}
